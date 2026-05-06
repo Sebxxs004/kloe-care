8fdf044c465775b315e314b9f1685ee64339eb88
Sebastian Cano <sebastiancano12560@gmail.com>
2026-05-05 19:25:22 -0500

    feat: base project

---
+++ src/test/java/com/universidad/kloe_care/KloeCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.kloe_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KloeCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
